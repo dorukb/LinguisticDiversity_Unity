3f8f34c288c8b9b367ee3b9d92d37b2fe4ca7c6b
Doruk Bildibay <dbildibay@gmail.com>
2021-08-24 17:57:04 +0300

    add recording cancellation time tolerance.
    + now the recording is stopped only if the recording button is released, and not clicked again for the tolerance duration ( 0.5secs as of now)

--- LanguageDiversityApp/Assets/Scripts/RecordingButton.cs
+++ LanguageDiversityApp/Assets/Scripts/RecordingButton.cs
@@ -1,21 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class RecordingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    // time(secs) to wait before stopping recording after release.
+    // if pointer Down happens during this time, no cancelletion occurs.
+    public float toleranceDuration = 0.5f;
+
+
     RecordingManager manager;
+
+    bool pointerDown = false;
+    Coroutine stoppingCR;
+
     void Awake()
     {
         manager = FindObjectOfType<RecordingManager>();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
+        pointerDown = true;
         manager.StartRecording();
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        manager.StopRecording();
-    }
+        pointerDown = false;
 
+        if(stoppingCR != null)
+        {
+            StopCoroutine(stoppingCR);
+        }
+        stoppingCR = StartCoroutine(MaybeStopRecording());
+    }
+    private IEnumerator MaybeStopRecording()
+    {
+        yield return new WaitForSeconds(toleranceDuration);
+
+        if (!pointerDown)
+        {
+            manager.StopRecording();
+        }
+        else
+        {
+            // pointer down occured during our wait, do not stop recording.
+        }
+    }
 }
